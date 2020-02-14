f29b49e8db6d38dd6be6470163ee3ce509bdd86d
kaushal prajapati <kaushal.prajapati@sap.com>
2020-02-14 15:12:26 -0800

    Two dimentional array

---
+++ Two_Dimentional_Array/chess_board_collision.py
@@ -0,0 +1,38 @@
+# if the pyada collide with each other in any row
+# input will be a mattrix with 0 and 1
+
+def rooks_are_sf
+
+def rooks_are_safe(input):
+    # row by row traversing
+    for i in range(len(input)): # column length
+        rowCount = 0
+        for j in range(len(input[i])): # row length
+            if input[i][j] == 1:
+                rowCount += 1
+                if rowCount > 1:
+                    return False
+                else:
+                    continue
+            
+    for i in range(len(input[0])): # row length
+        rowCount = 0
+        for j in range(len(input)): # column length
+            if input[j][i] == 1: # fix the column increase the row
+                rowCount += 1
+                if rowCount > 1:
+                    return False
+                else:
+                    continue
+
+    return True
+
+
+print(rooks_are_safe(input_no_collision))
+print(rooks_are_safe(input_collision))
+
+    
+
+
+
+        
