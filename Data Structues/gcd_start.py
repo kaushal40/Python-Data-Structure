961803f8d73fbc575c60965c0a0ac56cb72b0c23
Kaushal Prajapati <kaushalprajapati@Kaushals-MacBook-Air.local>
2020-05-02 18:43:24 -0700

    Code Fixes

--- Data Structues/gcd_start.py
+++ Data Structues/gcd_start.py
@@ -3,10 +3,10 @@
 
 def gcd(a, b):
     while (b!=0): 
-        t = a;
-        a = b;
-        b = t % b;
-    return a;
+        t = a
+        a = b
+        b = t % b
+    return a
 
 print(gcd(45, 210))  # should be 12
 print(gcd(20, 8))   # should be 4
